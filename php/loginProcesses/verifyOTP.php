dde5273425e5cd0d081af39c32b6914cf9d7b00d
alfredbenitez40@gmail.com <alfredbenitez40@gmail.com>
2021-09-23 20:18:44 +0800

    update with session and email OTP

--- php/loginProcesses/verifyOTP.php
+++ php/loginProcesses/verifyOTP.php
@@ -11,9 +11,12 @@ if($verificationType=="email"){
     $loginProcess  = new loginProcess();
     $isValidOTP =  $loginProcess->verifyOTP($con,$email,$inputtedOTP);
 
-    $_SESSION['email']=$email;
+    if($isValidOTP){
+        $_SESSION['email']=$email;
 
-    echo json_encode(array("result"=>$isValidOTP));
+        echo json_encode(array("result"=> true));
+    }
+
 
     //echo json_encode(array("name"=>$email,"time"=>$verificationType,"OTP"=>$isValidOTP));//pang debug lang
 }
