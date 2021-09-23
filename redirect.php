b862cbbe2d793c66418a6050c06569a30dd497f9
alfredbenitez40@gmail.com <alfredbenitez40@gmail.com>
2021-09-23 19:56:20 +0800

    update with session and email OTP

--- redirect.php
+++ redirect.php
@@ -1,4 +1,10 @@
 <?php
+//purpose of this code is to redirect the user to main
+//example:
+//user is admin -> will go to admin part
+//user is super admin -> will go to super admin part
+//user is patient -> will go to patient part
+//need to add some codes later
 session_start();
 header("location:main.php",true);
 exit();
