c566345bf6f2daa1bf69ea07824f54ad1c9bd8ad
Jay <selwynjayf@gmail.com>
2021-10-03 02:20:38 +0800

    about page in progess

--- index.php
+++ index.php
@@ -16,12 +16,6 @@ if(isset($_SESSION['email'])){
     <!--CSS Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <!--CSS Grid
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-4-grid@3.4.0/css/grid.min.css"
-    />-->
-
     <!--CSS Styling-->
     <link rel="stylesheet" href="../capstone-project/css/main.css"/>
 
