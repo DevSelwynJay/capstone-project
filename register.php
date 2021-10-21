672dc6a1a796f88fd88128e7f196b8d551c5031e
Alfredo Benitez <alfredogiebenitez@gmail.com>
2021-10-21 19:10:28 +0800

    add tooltip script in register and database table folder

--- register.php
+++ register.php
@@ -18,6 +18,9 @@
       <title>Register</title>
       <!--Jquery-->
       <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+      <!--Bootstrap JS-->
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
    </head>
    <body>
@@ -51,7 +54,7 @@
                         </div>
                         <div class="row">
                             <div class="col-sm-3">
-                                <select type="text">
+                                <select data-toggle="tooltip" data-placement="top" title="Purok" data-container="body">
                                     <option value="" disabled selected>Purok</option>
                                     <?php
                                     for($a=1;$a<=7;$a++){
@@ -60,13 +63,12 @@
                                     ?>
                                 </select>
                             </div>
-                           <div class="col-sm-3"><input type="number" placeholder="House #" /></div>
+                           <div class="col-sm-3"><input type="number" placeholder="House #" id="num" data-toggle="tooltip" data-placement="top" title="House #" data-container="body"/></div>
                            <div class="col-sm-6"><input type="text" placeholder="" value="Sto. Rosario" readonly/></div>
                         </div>
                          <div class="row">
                              <div class="col-sm-6"><input type="text" placeholder="" value="Paombong" readonly/></div>
                              <div class="col-sm-6"><input type="text" placeholder="" value="Bulacan" readonly/></div>
-
                          </div>
                         <a class="primary-btn" id="reg">Register</a>
                      </div>
@@ -82,6 +84,7 @@
           $(".logo").click(function (){
              location.href="index.php";
           })
+          $('[data-toggle="tooltip"]').tooltip()
       </script>
    </body>
 </html>
