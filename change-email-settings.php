23c3b347979899209b8242870a4f8d81d542cae5
alfredogie11 <87077926+alfredogie11@users.noreply.github.com>
2021-11-16 16:25:29 +0800

    add drop down super admin

--- change-email-settings.php
+++ change-email-settings.php
@@ -28,7 +28,7 @@ if(!isset($_SESSION['email'])){
     <!--Jquery-->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
-    <!--Jquery UI css and js
+    <!--Jquery UI css and js-->
     <link rel="stylesheet" href="jquery-ui/jquery-ui.css">
     <link rel="stylesheet" href="scss/tooltip.css">
     <script src="jquery-ui/jquery-ui.js"></script>
@@ -47,7 +47,7 @@ if(!isset($_SESSION['email'])){
                 }
             }
         });
-    </script>-->
+    </script>
 
     <!--Bootstrap
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
