1adfabf774c0f749891050c0b484d8706a33920a
daks ako <benitez.alfredo.b.1128@gmail.com>
2021-12-22 16:33:50 +0800

    patient side partially done

--- php/logPatientAccShortcut.php
+++ php/logPatientAccShortcut.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION['email'] = "ben@gmail.com";
+$_SESSION['email'] = "benitez.alfredo.b.1128@gmail.com";
 //alfredogiebenitez@gmail.com//admin
 //capstoneproject3m@gmail.com//superadmin
 //$_SESSION['user_full_name']="Alfredo Sample";
--- php/patientSide/patientSession.php
+++ php/patientSide/patientSession.php
@@ -1,7 +1,8 @@
 <?php
-session_start();//sa patient side
+session_start();
 $con = null;
 require '../DB_Connect.php';
+
 if(!isset($_SESSION['email'])||$_SESSION['account_type']!=2){
     echo json_encode(array(
         "patient_name"=>"no logged patient"
@@ -9,13 +10,28 @@ if(!isset($_SESSION['email'])||$_SESSION['account_type']!=2){
     exit();
 }
 $email = $_SESSION['email'];
-$res = mysqli_query($con,"SELECT*FROM patient where email = '$email' ");
+$res = mysqli_query($con,"SELECT *, timestampdiff(year ,birthday,NOW()) as age, DATE_FORMAT(birthday,'%M %e, %Y') as bday FROM patient where email = '$email' ");
+if(mysqli_num_rows($res)<=0){
+    echo json_encode(array(
+        "patient_name"=>"invalid patient email"
+    ));
+    exit();
+}
 if($res){
     if($row = mysqli_fetch_assoc($res)){
 
+        //will use to check if there the same record in walk in patient db
+        $_SESSION['pfname'] = $row['first_name'];
+        $_SESSION['plname'] = $row['last_name'];
+        $_SESSION['pmname'] = $row['middle_name'];
+        $_SESSION['pbday'] = $row['birthday'];
+        $_SESSION['ppurok'] = $row['purok'];
+
         echo json_encode(array(
             "patient_id"=>$row['id']
-        ,"patient_name"=>$row['first_name']." ".$row['middle_name']." ".$row['last_name']
+            ,"patient_name"=>$row['first_name']." ".$row['middle_name']." ".$row['last_name']
+            ,"age"=>$row['age'],"gender"=>$row['gender'],"birthday"=>$row['bday'],
+            "contact_no"=>$row['contact_no'], "address"=>"Purok ".$row['purok'].", ".$row['house_no']." ".$row['address']
         ));
 
     }
