422e39d202d811d34bf4c61cd7151a74854fd71d
daks ako <benitez.alfredo.b.1128@gmail.com>
2021-12-24 11:49:50 +0800

    fix bugs in patient side days of interval

--- php/patientSide/retrieveCurrentMedication.php
+++ php/patientSide/retrieveCurrentMedication.php
@@ -8,14 +8,41 @@ require '../DB_Connect.php';
 
 $arr = array();
 //get the end of medication date that is from the future
-$query = "SELECT *, timestampdiff(day,start_date,end_date)+1 as duration_days,
+$query = "SELECT *, 
        DATE_FORMAT(start_date,'%b %d, %Y') as start_date, DATE_FORMAT(end_date,'%b %d, %Y') as end_date
 FROM medication_record WHERE patient_id = '$patient_id' AND end_date >= NOW() ";
 
 $result = mysqli_query($con,$query);
 while($row = mysqli_fetch_assoc($result) ){
-    $arr[] = $row;
+
+    $duration_days = getDuration($row['start_date'],$row['end_date'],$row['interval_days']+1);
+
+    //$arr[] = $row;
+    $arr[] = array(
+        "medicine_name"=>$row['medicine_name'],
+        "dosage"=>$row['dosage'],
+        "no_times"=>$row['no_times'],
+        "duration_days"=>$duration_days,
+        "end_date"=>$row['end_date']
+
+    );
 }
 
 echo json_encode($arr);
 mysqli_close($con);
+
+function getDuration($start, $end, $interval=0,$format = 'Y-m-d') :int{
+    $array = array();
+    $interval = new DateInterval('P'.$interval.'D');
+
+    $realEnd = new DateTime($end);
+    $realEnd->add($interval);
+
+    $period = new DatePeriod(new DateTime($start), $interval, $realEnd);
+
+    foreach($period as $date) {
+        $array[] = $date->format($format);
+    }
+
+    return count($array);
+}
