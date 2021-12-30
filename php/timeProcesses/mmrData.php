6e0e9e598afa8df61b915ecd5948c474def0f075
rshnclglvz <irishnicole.galvez.d@bulsu.edu.ph>
2021-12-30 18:34:33 +0800

    track covid and mmr done as well.

---
+++ php/timeProcesses/mmrData.php
@@ -0,0 +1,146 @@
+<?php
+//trial palang wala pa to
+$con=null;
+require '../DB_Connect.php';
+
+if(!$con){
+    die("Error".mysqli_error($con));
+    exit();
+}
+
+$count = $_POST["ctr"];
+
+if($count==1){
+    //INFANT
+    $mmrArr = array();
+    $number1 = array();
+    $results1 = array();
+    $ctr = 0;$cc=1;
+//minor
+    $number = array();
+    $results = array();
+    while ($cc<8){
+        $results1[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Infant' 
+                                          AND vaccine_name='MMR' AND WEEKOFYEAR(date_vaccinated) = WEEKOFYEAR(NOW()) group by patient_id");
+        $number1[$ctr] = mysqli_num_rows($results1[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+    $mmrArr["infdata"]= $number1;
+
+    $ctr = 0;$cc=1;
+    while ($cc<8){
+        $results[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Minor'
+                                            AND vaccine_name='MMR' AND WEEKOFYEAR(date_vaccinated) = WEEKOFYEAR(NOW()) group by patient_id");
+        $number[$ctr] = mysqli_num_rows($results[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+
+    $mmrArr["mindata"] = $number;
+
+    $json = json_encode($mmrArr);
+    echo $json;
+
+} elseif ($count==2) {// 2 signifies that month button is clicked
+    //INFANT
+    $mmrArr = array();
+    $number1 = array();
+    $results1 = array();
+    $ctr = 0;$cc=1;
+//minor
+    $number = array();
+    $results = array();
+    while ($cc<8){
+        $results1[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Infant' 
+                                          AND vaccine_name='MMR' AND MONTH(date_vaccinated)=MONTH(now())
+                                          AND YEAR(date_vaccinated)=YEAR(now()) group by patient_id");
+        $number1[$ctr] = mysqli_num_rows($results1[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+    $mmrArr["infdataMo"]= $number1;
+
+    $ctr = 0;$cc=1;
+    while ($cc<8){
+        $results[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Minor'
+                                            AND vaccine_name='MMR' AND MONTH(date_vaccinated)=MONTH(now())
+                                            AND YEAR(date_vaccinated)=YEAR(now()) group by patient_id");
+        $number[$ctr] = mysqli_num_rows($results[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+
+    $mmrArr["mindataMo"] = $number;
+
+    $json = json_encode($mmrArr);
+    echo $json;
+
+}elseif ($count==3) {// 3 signifies that year button is clicked
+    //INFANT
+    $mmrArr = array();
+    $number1 = array();
+    $results1 = array();
+    $ctr = 0;$cc=1;
+//minor
+    $number = array();
+    $results = array();
+    while ($cc<8){
+        $results1[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Infant' 
+                                          AND vaccine_name='MMR' AND YEAR(date_vaccinated)=YEAR(now()) group by patient_id");
+        $number1[$ctr] = mysqli_num_rows($results1[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+    $mmrArr["infdataYr"]= $number1;
+
+    $ctr = 0;$cc=1;
+    while ($cc<8){
+        $results[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Minor'
+                                            AND vaccine_name='MMR' AND YEAR(date_vaccinated)=YEAR(now()) group by patient_id");
+        $number[$ctr] = mysqli_num_rows($results[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+
+    $mmrArr["mindataYr"] = $number;
+
+    $json = json_encode($mmrArr);
+    echo $json;
+
+}elseif ($count==4) {// 4 signifies that overall button is clicked
+    //INFANT
+    $mmrArr = array();
+    $number1 = array();
+    $results1 = array();
+    $ctr = 0;$cc=1;
+//minor
+    $number = array();
+    $results = array();
+    while ($cc<8){
+        $results1[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Infant' 
+                                          AND vaccine_name='MMR' group by patient_id");
+        $number1[$ctr] = mysqli_num_rows($results1[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+    $mmrArr["infdataOv"]= $number1;
+
+    $ctr = 0;$cc=1;
+    while ($cc<8){
+        $results[$ctr] = mysqli_query($con, "SELECT event_id FROM vaccination_record WHERE patient_purok='$cc' AND patient_type='Minor'
+                                            AND vaccine_name='MMR' group by patient_id");
+        $number[$ctr] = mysqli_num_rows($results[$ctr]);
+        $ctr++;$cc++;
+    }
+    $ctr=0;
+
+    $mmrArr["mindataOv"] = $number;
+
+    $json = json_encode($mmrArr);
+    echo $json;
+}
+
+
+
+?>
