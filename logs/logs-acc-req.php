a0fda3c40550c2687898f12ae0647673fbafe616
daks ako <benitez.alfredo.b.1128@gmail.com>
2022-01-20 22:49:25 +0800

    fix some query bugs in acc request logs

--- logs/logs-acc-req.php
+++ logs/logs-acc-req.php
@@ -7,11 +7,13 @@ $res = mysqli_query($con,"SELECT * FROM logs_acc_req ORDER by date_occured DESC"
 while($row = mysqli_fetch_assoc($res)){
 
     //get admin info who take that action
+    $row['admin_name']="Unknown";
     $queryAdminName = mysqli_query($con,"SELECT * FROM admin WHERE id = '". $row["admin_id"]. "'");
     if($r1 = mysqli_fetch_assoc($queryAdminName)){
         $row['admin_name'] = $r1['first_name']." ".$r1['middle_name']." ".$r1['last_name'];
     }
     //get patient name
+    $row['patient_name']="Unknown";
     $status = $row['admin_action'];// 0 declines 1 approved//varchar ' '
     if($status=='0'){
         $queryPatientName = mysqli_query($con,"SELECT * FROM declined_patient WHERE id = '". $row['patient_id']. "'");
