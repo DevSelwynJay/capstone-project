dd51f573546c0c90b44e9190ce75f53d1964dfef
Lowell <SpyWelll>
2022-02-13 09:01:26 +0800

    recommendations working on it

--- php/inventoryP/displayMeds2.php
+++ php/inventoryP/displayMeds2.php
@@ -5,7 +5,7 @@ require '../DB_Connect.php';
 $arr=array();
 $res = mysqli_query($con,"Select id, CONCAT(name,'(',dosage,')') as name, stock from `medinventory` where `expdate` < NOW() and `stock` > `criticalstock` order by `dateadded` asc");
 while($row=mysqli_fetch_assoc($res)){
-    $row['button'] ='<i id="exclamation" class="fas fa-trash deletebtnt" data-id="'.$row['id'].'"  ></i>';
+    $row['button'] ='<i class="fa-light fa-circle-exclamation"></i>';
     $arr[]=$row;
 }
 echo json_encode($arr);
