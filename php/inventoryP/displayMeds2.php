5da3066ca90db7cbd968d4dd1f33e4671a96d537
Lowell <SpyWelll>
2022-02-13 14:36:01 +0800

    recommendations working on it

--- php/inventoryP/displayMeds2.php
+++ php/inventoryP/displayMeds2.php
@@ -3,7 +3,7 @@ session_start();
 $con=null;
 require '../DB_Connect.php';
 $arr=array();
-$res = mysqli_query($con,"Select id, CONCAT(name,'(',dosage,')') as name, stock from `medinventory` where `expdate` < NOW() and `stock`< `criticalstock` order by `dateadded` asc");
+$res = mysqli_query($con,"Select id, CONCAT(name,'(',dosage,')') as name, stock from `medinventory` where `expdate` > NOW() and `stock` <= `criticalstock` order by `dateadded` asc");
 while($row=mysqli_fetch_assoc($res)){
     if($row['stock']==1){
         $row['stock']= $row['stock'].' pc';
